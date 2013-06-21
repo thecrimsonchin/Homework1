392ecca0fd7e96a193ddda9e14d1216e21734851
Ubuntu <ubuntu@ip-172-31-18-180.us-west-1.compute.internal>
2013-06-21 17:39:19 +0000

    added homework1.js

---
+++ homework1.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var outfile = "hello.txt";
+var out = "YAY IT WORKS!.\n";
+fs.writeFileSync(outfile, out);  
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
+
